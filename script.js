a19426075a464e06f1fb19f4b7c1adbdebd97a7f
Hubert <108083851+Hubixonek@users.noreply.github.com>
2023-01-04 20:23:14 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const amount = document.querySelector('#amount')
+const course = document.querySelector('#course')
+const currencyOne = document.querySelector('.inputGroupSelect01')
+const currencyTwo = document.querySelector('.inputGroupSelect02')
+const convBtn = document.querySelector('.btn btn-primary')
+const result = document.querySelector('#result')
+const saveBtn = document.querySelector('#save-btn')
+
+const calculation = () =>{
+   
+    
+
+    if(amount.value==''){
+        result.value = 'Wpisz kwotę!';
+        result.style.color = 'red';
+        }else if(course.value==''){
+            result.value = 'Wpisz kurs!';
+            result.style.color = 'red';
+        }else{
+            result.style.color = 'gold'
+            result.value = amount.value * course.value
+        }
+    }
+
+amount.addEventListener('keyup', calculation)
+course.addEventListener('keyup', calculation)
+
