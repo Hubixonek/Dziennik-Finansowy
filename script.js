38434bf9026de06782ab834949cbaeb1b6f14041
Hubert Adamkowski <hubertadamkowskip56@gmail.com>
2023-01-08 20:15:54 +0100

    Napisałem funkcje gdzie mnoży się wynik przez podany kurs i otrzymujemy dzięki temu kwotę. Poprawiłem pierwszy skrypt kalkulacji kwoty x kurs = wynik, dodając classList.add w zastępstwie za style.color. Poprawiłem front-end by był przyjemniejszy dla oka i 1:1 względem rysunku, szkicu projektu

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const amount = document.querySelector('#amount');
 const course = document.querySelector('#course');
-const currencyOne = document.querySelector('.inputGroupSelect01');
-const currencyTwo = document.querySelector('.inputGroupSelect02');
-const convBtn = document.querySelector('.btn btn-primary');
 const result = document.querySelector('#result');
 const saveBtn = document.querySelector('#save-btn');
+const currencyOne = document.querySelector('#inputGroupSelect01');
+const currencyTwo = document.querySelector('#inputGroupSelect02');
+const convBtn = document.querySelector('.btn btn-primary');
 
+// Multiplying amount and course to get result
 const calculation = () => {
   if (amount.value === '') {
     result.value = 'Wpisz kwotę!';
@@ -14,10 +15,19 @@ const calculation = () => {
     result.value = 'Wpisz kurs!';
     result.classList.add('text-red');
   } else {
-    result.style.color = 'gold';
+    result.classList.remove('text-red');
+    result.classList.add('text-lime');
     result.value = amount.value * course.value;
   }
 };
+// Multiplying the result by the curse
+const secondCalculation = () => {
+  if (result.value === '') {
+  } else {
+    amount.value = result.value * course.value;
+  }
+};
 
+result.addEventListener('keyup', secondCalculation);
 amount.addEventListener('keyup', calculation);
 course.addEventListener('keyup', calculation);
