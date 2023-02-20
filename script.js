398cedfc417a4758ad561038cc7f0dee816b0352
Hubert Adamkowski <hubertadamkowskip56@gmail.com>
2023-02-20 20:08:35 +0100

    Kolejne zmianki z localstorage

--- script.js
+++ script.js
@@ -1,137 +1,123 @@
-const amount = document.getElementById('amount');
-const course = document.getElementById('course');
-const result = document.getElementById('result');
-const currencyOne = document.getElementById('currencyOne');
-const currencyTwo = document.getElementById('currencyTwo');
-const currencyText = document.getElementById('currencyText');
-const date = document.getElementById('date');
-const saveBtn = document.getElementById('save-btn');
-const table = document.querySelector('.table-test');
-const tdList = document.querySelector('.saved-datas');
-const tr = document.querySelector('.table test tr');
-// const tBodyEl = document.querySelector('tbody');
+(() => {
+  const amount = document.getElementById('amount');
+  const course = document.getElementById('course');
+  const result = document.getElementById('result');
+  const currencyOne = document.getElementById('currencyOne');
+  const currencyTwo = document.getElementById('currencyTwo');
+  const currencyText = document.getElementById('currencyText');
+  const date = document.getElementById('date');
+  const saveBtn = document.getElementById('save-btn');
+  const table = document.querySelector('.table-test');
+  const tdList = document.querySelector('.saved-datas');
+  const history = JSON.parse(window.localStorage.getItem('history')) || [];
+  const API_LINK = 'http://api.nbp.pl/api/exchangerates/tables/C/';
+  // const tBodyEl = document.querySelector('tbody');
 
-const API_LINK = 'http://api.nbp.pl/api/exchangerates/tables/C/';
-
-async function fetchRates() {
-  try {
-    const response = await fetch(API_LINK);
-    const data = await response.json();
-    const currencys = data[0].rates;
-    console.log(currencys);
-    // znalezc obiekt za pomoca property
-  } catch {
-    console.error('błąd');
+  async function fetchRates() {
+    try {
+      const response = await fetch(API_LINK);
+      const data = await response.json();
+      const currencys = data[0].rates;
+      console.log(currencys);
+      // znalezc obiekt za pomoca property
+    } catch {
+      console.error('błąd');
+    }
   }
-}
-fetchRates();
-
-// const main = () =>{
-
-// }
-
-// Multiplying amount and course to get result
-const calculation = () => {
-  if (amount.value === '') {
-    result.value = 'Wpisz kwotę!';
-    result.classList.remove('text-lime');
-    result.classList.add('text-red');
-  } else if (course.value === '') {
-    result.value = 'Wpisz kurs!';
-    result.classList.remove('text-lime');
-    result.classList.add('text-red');
-  } else {
-    result.classList.remove('text-red');
-    result.classList.add('text-lime');
-    result.value = amount.value * course.value;
-  }
-};
-// Multiplying the result by the curse
-const secondCalculation = () => {
-  if (result.value !== '') {
-    amount.value = result.value * course.value;
-  }
-};
-currencyOne.addEventListener('change', (event) => {
-  currencyText.textContent = `${event.target.value}`;
-  currencyTwo.addEventListener('change', (e) => {
-    currencyText.textContent = `${event.target.value}/${e.target.value}`;
+  fetchRates();
+  // Multiplying amount and course to get result
+  const calculation = () => {
+    if (!amount.value) {
+      result.value = 'Wpisz kwotę!';
+      result.classList.remove('text-lime');
+      result.classList.add('text-red');
+    } else if (!course.value) {
+      result.value = 'Wpisz kurs!';
+      result.classList.remove('text-lime');
+      result.classList.add('text-red');
+    } else {
+      result.classList.remove('text-red');
+      result.classList.add('text-lime');
+      result.value = amount.value * course.value;
+    }
+  };
+  // Multiplying the result by the curse
+  const secondCalculation = () => {
+    if (result.value) {
+      amount.value = result.value * course.value;
+    }
+  };
+  const updateCurrencyText = () => {
+    currencyText.textContent = `${currencyOne.value}/${currencyTwo.value}`;
+  };
+  // Swap positions between currencyOne and currencyTwo
+  let temp = '';
+  currencyTwo.addEventListener('click', (event) => {
+    temp = event.target.value;
   });
-});
-// Swap positions between currencyOne and currencyTwo
-let temp = '';
-currencyTwo.addEventListener('click', (e) => {
-  // if(currencyOne.value === currencyTwo.value)
-  temp = e.target.value;
-});
-currencyTwo.addEventListener('change', (e) => {
-  if (currencyTwo.value === currencyOne.value) {
-    currencyOne.value = temp;
+  currencyTwo.addEventListener('change', (event) => {
+    if (event.target.value === currencyOne.value) {
+      currencyOne.value = temp;
+      updateCurrencyText();
+    }
+  });
+  let secondTemp = '';
+  currencyOne.addEventListener('click', (event) => {
+    secondTemp = event.target.value;
+  });
+  currencyOne.addEventListener('change', (event) => {
+    if (event.target.value === currencyTwo.value) {
+      currencyTwo.value = secondTemp;
+      updateCurrencyText();
+    }
+  });
+
+  const addTableRow = (item) => {
+    const newRow = table.insertRow(table.length);
+    const dateCell = newRow.insertCell(0);
+    const amountCell = newRow.insertCell(0);
+    const currencyCell = newRow.insertCell(0);
+    const courseCell = newRow.insertCell(0);
+    const resultCell = newRow.insertCell(0);
+
+    resultCell.innerHTML = item.date;
+    courseCell.innerHTML = item.amount;
+    currencyCell.innerHTML = item.currency;
+    amountCell.innerHTML = item.course;
+    dateCell.innerHTML = item.result;
+
+    tdList.classList.add('addDisplay');
+  };
+
+  function saveDatas() {
+    const datas = {
+      date: date.value,
+      amount: amount.value,
+      currency: currencyText.innerHTML,
+      course: course.value,
+      result: result.value,
+    };
+
+    history.push(datas);
+    window.localStorage.setItem('history', JSON.stringify(history));
+    addTableRow(datas);
   }
-});
-let secondTemp = '';
-currencyOne.addEventListener('click', (e) => {
-  secondTemp = e.target.value;
-});
-currencyOne.addEventListener('change', (e) => {
-  if (currencyOne.value === currencyTwo.value) {
-    currencyTwo.value = secondTemp;
-    currencyText.textContent = `${currencyOne.value}/${secondTemp}`;
-  }
-});
 
-function getDatas() {
-  localStorage.setItem('date', JSON.stringify(date.value));
-  localStorage.setItem('amount', JSON.stringify(amount.value));
-  localStorage.setItem('currency', JSON.stringify(currencyText.innerHTML));
-  localStorage.setItem('course', JSON.stringify(course.value));
-  localStorage.setItem('result', JSON.stringify(result.value));
-}
-function saveDatas() {
-  let cell1;
-  let cell2;
-  let cell3;
-  let cell4;
-  let cell5 = '';
-
-  let newRow = '';
-  const dateData = JSON.parse(localStorage.getItem('date'));
-  const amountData = JSON.parse(localStorage.getItem('amount'));
-  const currencyData = JSON.parse(localStorage.getItem('currency'));
-  const courseData = JSON.parse(localStorage.getItem('course'));
-  const resultData = JSON.parse(localStorage.getItem('result'));
-  tdList.classList.add('addDisplay');
-  newRow = table.insertRow(table.length);
-
-  cell5 = newRow.insertCell(0);
-  cell4 = newRow.insertCell(0);
-  cell3 = newRow.insertCell(0);
-  cell2 = newRow.insertCell(0);
-  cell1 = newRow.insertCell(0);
-
-  cell5.innerHTML = resultData;
-  cell4.innerHTML = courseData;
-  cell3.innerHTML = currencyData;
-  cell2.innerHTML = amountData;
-  cell1.innerHTML = dateData;
-
-  if (localStorage.getItem('history') == null) {
-    localStorage.setItem('history', '[]');
-  }
-  const history = JSON.parse(localStorage.getItem('history'));
-  history.push(dateData);
-  localStorage.setItem('history', JSON.stringify(history));
-
-  if (localStorage.getItem('history') != null) {
-    cell1.innerHTML = JSON.parse(localStorage.getItem('history'));
-  }
-}
-window.onload = function () {
-  saveDatas();
-  getDatas();
-};
-saveBtn.addEventListener('click', getDatas);
-saveBtn.addEventListener('click', saveDatas);
-result.addEventListener('keyup', secondCalculation);
-amount.addEventListener('keyup', calculation);
-course.addEventListener('keyup', calculation);
+  window.addEventListener('load', () => {
+    const dataObject = localStorage.getItem('history');
+    if (!dataObject) {
+      return;
+    }
+    const parsedData = JSON.parse(dataObject);
+    parsedData.forEach(addTableRow);
+  });
+  const prepareDOMEvents = () => {
+    saveBtn.addEventListener('click', saveDatas);
+    result.addEventListener('keyup', secondCalculation);
+    amount.addEventListener('keyup', calculation);
+    course.addEventListener('keyup', calculation);
+    currencyOne.addEventListener('change', updateCurrencyText);
+    currencyTwo.addEventListener('change', updateCurrencyText);
+  };
+  prepareDOMEvents();
+})();
